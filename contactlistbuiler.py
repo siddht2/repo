79d0c8a66f39ebc1443c684e1bb4b440ab33a802
technowizard <aheisleycook@gmail.com>
2017-12-26 01:04:26 -0800

    added  email entry

--- contactlistbuiler.py
+++ contactlistbuiler.py
@@ -5,7 +5,7 @@ class Contactcreator:
     def __init__(self):
         #setup environment
         self.Font = ["arial 16"]
-        root.geometry("200x200")
+        root.geometry("200x280")
         root.title("contact creator")
         ttk.Style().configure("TLabel", relief="raised",background="#0ff", font=self.Font)
         ttk.Style().configure("TEntry", relief="raised",background="#aff", font=self.Font)
@@ -22,22 +22,23 @@ class Contactcreator:
         self.phonenumber.pack(side="top")
         self.number = ttk.Entry()
         self.number.pack()
+        self.Email = ttk.Label(text="email address")
+        self.Email.pack(side="top")
+        self.email = ttk.Entry()
+        self.email.pack()
         self.Save = ttk.Button(text="save data", command=self.save)
         self.Save.pack(side="top")
     """main function for creating contact data"""
+    
     def save(self):
         with open(self.filename.get(), "a", newline='') as csvfile:
-            self.fieldnames = ['name',  "phone-number"]
+            self.fieldnames = ['name', "email-address","phonenumber"]
             self.writer =  csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             
             self.writer.writeheader()
-            self.writer.writerow({'name':self.name.get(), "phone-number":self.number.get()})
+            self.writer.writerow({'name':self.name.get(), "email-address":self.email.get(), "phonenumber":self.number.get()})
 
-        
-
-
-
-
+       
 
         
 root = Tk()  
