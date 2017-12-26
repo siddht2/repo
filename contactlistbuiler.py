efa95a68aa801a616299a2aae785bd68ffa9b7a1
technowizard <aheisleycook@gmail.com>
2017-12-26 00:32:08 -0800

    contact lister builder

---
+++ contactlistbuiler.py
@@ -0,0 +1,45 @@
+import csv
+from tkinter import * 
+from tkinter import ttk
+class Contactcreator:
+    def __init__(self):
+        #setup environment
+        self.Font = ["arial 16"]
+        root.geometry("200x200")
+        root.title("contact creator")
+        ttk.Style().configure("TLabel", relief="raised",background="#0ff", font=self.Font)
+        ttk.Style().configure("TEntry", relief="raised",background="#aff", font=self.Font)
+        ttk.Style().configure("TButton", relief="raised", background="#aff", font=self.Font)
+        self.flabel = ttk.Label(text="filename")
+        self.flabel.pack(side="top")
+        self.filename = ttk.Entry()
+        self.filename.pack(side="top")
+        self.contactname = ttk.Label(text="name")
+        self.contactname.pack(side="top")
+        self.name = ttk.Entry()
+        self.name.pack()
+        self.phonenumber = ttk.Label(text="phonenumber:") 
+        self.phonenumber.pack(side="top")
+        self.number = ttk.Entry()
+        self.number.pack()
+        self.Save = ttk.Button(text="save data", command=self.save)
+        self.Save.pack(side="top")
+    """main function for creating contact data"""
+    def save(self):
+        with open(self.filename.get(), "a", newline='') as csvfile:
+            self.fieldnames = ['name',  "phone-number"]
+            self.writer =  csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            
+            self.writer.writeheader()
+            self.writer.writerow({'name':self.name.get(), "phone-number":self.number.get()})
+
+        
+
+
+
+
+
+        
+root = Tk()  
+contactcreator = Contactcreator()
+root.mainloop()
