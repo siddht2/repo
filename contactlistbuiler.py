a9f9ce0685ca6de61472dd1646596a1ab0f0b23e
technowizard <aheisleycook@gmail.com>
2017-12-27 23:25:04 -0800

    added menu and changed format

--- contactlistbuiler.py
+++ contactlistbuiler.py
@@ -1,35 +1,46 @@
 import csv
 from tkinter import * 
 from tkinter import ttk
+from sys import *
+from tkinter import messagebox as mbx
 class Contactcreator:
     def __init__(self):
-        #setup environment
-        self.Font = ["arial 16"]
+        """setup environment"""
+        self.Font = ["verdena 14"]
+        self.Bsize = ["verdena 12"]
         root.geometry("200x280")
         root.title("contact creator")
-        ttk.Style().configure("TLabel", relief="raised",background="#0ff", font=self.Font)
+        root.resizable(0,0)
+        ttk.Style().configure("TFrame", )
+        ttk.Style().configure("TLabel", relief="raised", background="#0ff", font=self.Font)
         ttk.Style().configure("TEntry", relief="raised",background="#aff", font=self.Font)
-        ttk.Style().configure("TButton", relief="raised", background="#aff", font=self.Font)
-        self.flabel = ttk.Label(text="filename")
+        ttk.Style().configure("TButton", relief="raised", background="#aff", font=self.Bsize)
+        self.lsize = 10
+        self.esize = 18
+        menu = Menu(root)
+        root.config(menu=menu)
+        filemenu = Menu(menu)
+        menu.add_command(label="about", command=self.about)
+        menu.add_command(label="exit", command=root.quit)
+        self.flabel = ttk.Label(text="filename", width=self.lsize)
         self.flabel.pack(side="top")
-        self.filename = ttk.Entry()
+        self.filename = ttk.Entry(width=self.esize)
         self.filename.pack(side="top")
-        self.contactname = ttk.Label(text="name")
+        self.contactname = ttk.Label(text="name", width=self.lsize)
         self.contactname.pack(side="top")
-        self.name = ttk.Entry()
+        self.name = ttk.Entry(width=self.esize)
         self.name.pack()
-        self.phonenumber = ttk.Label(text="phonenumber:") 
+        self.phonenumber = ttk.Label(text="phone#", width=self.lsize) 
         self.phonenumber.pack(side="top")
-        self.number = ttk.Entry()
+        self.number = ttk.Entry(width=self.esize)
         self.number.pack()
-        self.Email = ttk.Label(text="email address")
+        self.Email = ttk.Label(text="email", width=self.lsize)
         self.Email.pack(side="top")
-        self.email = ttk.Entry()
+        self.email = ttk.Entry(width=self.esize)
         self.email.pack()
         self.Save = ttk.Button(text="save data", command=self.save)
         self.Save.pack(side="top")
     """main function for creating contact data"""
-    
     def save(self):
         with open(self.filename.get(), "a", newline='') as csvfile:
             self.fieldnames = ['name', "email-address","phonenumber"]
@@ -38,7 +49,12 @@ class Contactcreator:
             self.writer.writeheader()
             self.writer.writerow({'name':self.name.get(), "email-address":self.email.get(), "phonenumber":self.number.get()})
 
-       
+    def about(self):
+        mbx.showinfo("about","created by austin Heisley-Cook.\n\nyear:2017")
+
+
+
+
 
         
 root = Tk()  
